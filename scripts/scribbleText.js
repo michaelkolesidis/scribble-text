d9ac503b49840f2eb12e5af96baac692893efc85
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-05-12 00:47:31 +0300

    added script

---
+++ scripts/scribbleText.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Michael Kolesidis
+ * MIT License
+ * 
+ */
+
+let x, y, newX, newY;
+
+function setup() {
+  createCanvas(
+    windowWidth - (windowWidth * 0.3) / 100,
+    windowHeight - (windowHeight * 0.4) / 100
+  );
+  background(255);
+  x = width / 2;
+  y = height / 2;
+}
+
+function draw() {
+  strokeWeight(4);
+
+  newX = random(-50, width + 50);
+  newY = random(-50, height + 50);
+  line(x, y, newX, newY);
+  x = newX;
+  y = newY;
+}
