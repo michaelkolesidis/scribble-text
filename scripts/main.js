c21086627da03582123bb01f4b9f2ff2803d4211
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-03-30 10:46:49 +0300

    color switch correction

--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,19 @@
 
 let color = "white";
 let message = document.getElementById("message");
+let read = document.getElementById("read");
+
 message.style.color = color;
+read.style.color = color;
 
 document.addEventListener("click", () => {
   if (color === "white") {
     color = "black";
     message.style.color = color;
+    read.style.color = color;
   } else if (color === "black") {
     color = "white";
     message.style.color = color;
-  } else if (color === "black") {
-    color = "white";
-    message.style.color = color;
-  } else if (color === "black") {
-    color = "white";
-    message.style.color = color;
+    read.style.color = color;
   }
 });
